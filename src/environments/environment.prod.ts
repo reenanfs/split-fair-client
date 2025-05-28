f7d842dfebaa857b2ee5ebfe35c30894e55de6ed
reenanfs <renan.frsantana@gmail.com>
2025-05-27 22:02:47 -0300

    feat: updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://2vy79fr6f2.execute-api.sa-east-1.amazonaws.com',
+  apiUrl: 'https://q4g3y600mc.execute-api.sa-east-1.amazonaws.com',
   awsRegion: 'sa-east-1',
   amplifyConfig: {
-    user_pool_id: 'sa-east-1_vmeUKvAuH',
-    user_pool_client_id: '5tct3m8itter92bhmo8dq3b554',
+    user_pool_id: 'sa-east-1_mLDAnEvZt',
+    user_pool_client_id: '23gu01chh16hp1rc4ul7bhl6cu',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://2vy79fr6f2.execute-api.sa-east-1.amazonaws.com',
+  apiUrl: 'https://bf5ywclbtf.execute-api.sa-east-1.amazonaws.com',
   awsRegion: 'sa-east-1',
   amplifyConfig: {
-    user_pool_id: 'sa-east-1_vmeUKvAuH',
-    user_pool_client_id: '5tct3m8itter92bhmo8dq3b554',
+    user_pool_id: 'sa-east-1_U9uyVt5Dr',
+    user_pool_client_id: '17c4l5g7mn70igmijo0u3i0p86',
   },
 };
